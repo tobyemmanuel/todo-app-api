c2040ebb95fe59ba7ed4e54bebbe08da9c9028fe
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-07-01 15:11:07 +0100

    Full updates

--- routes/todoAppRoute.js
+++ routes/todoAppRoute.js
@@ -8,5 +8,4 @@ router.get('/fetchTasks', controller.fetchTasks) //fetch all Tasks via GET
 router.get('/fetchTask/:id', controller.fetchTask) //fetch a Task via GET
 router.put('/updateTask/:id', controller.updateTask) //update Task changes via PUT
 router.delete('/deleteTask/:id', controller.deleteTask) //delete Task via DELETE
-router.delete('/deleteAllTasks', controller.deleteAllTasks) //delete Task via DELETE
 module.exports = router;
